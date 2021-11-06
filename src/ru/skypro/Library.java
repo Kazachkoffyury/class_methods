4246b3081033ed24859739cc38a0dc0b682309f4
Kazackoffyury <kazachkoff.yury@yandex.ru>
2021-11-06 22:05:29 +0300

    ready

---
+++ src/ru/skypro/Library.java
@@ -0,0 +1,58 @@
+package ru.skypro;
+
+public class Library {
+    private Book[] books;
+
+    public Library(int countBook) {
+        this.books = new Book[countBook];
+    }
+
+    public void addBook(Book book) {
+        for (int i = 0; i < books.length; i++) {
+            if (books[i] == null) {
+                books[i] = book;
+                return;
+
+            }
+
+        }
+
+    }
+
+    public void printBook(String name) {
+        for (Book book : books) {
+            if (book.getName().equalsIgnoreCase(name)) {
+                System.out.println("book = " + book.getName() + ":" + book.getAuthor().getFoolName() + ":" + book.getYearPublishing());
+                return;
+
+            }
+
+        }
+        System.out.println("нет такой книги");
+
+
+    }
+
+    public void setYearPublishing(String name, int year) {
+        for (Book book : books) {
+            if (book.getName().equalsIgnoreCase(name)) {
+                book.setYearPublishing(year);
+                return;
+
+            }
+
+        }
+        System.out.println("книга не найдена!");
+
+    }
+
+
+
+
+    public void printBooks() {
+        for (Book book : books) {
+            System.out.println("Книга:" + " " + book.toString());
+
+        }
+    }
+}
